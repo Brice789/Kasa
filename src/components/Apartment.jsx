2dda98001d1d394c2fb38fc18effa8a60a654408
Brice789 <kireche.brice@gmail.com>
2023-08-09 16:38:51 +0200

    homepage finish

--- src/components/Apartment.jsx
+++ src/components/Apartment.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import "./Apartment.scss"
 import { Link } from 'react-router-dom'
 
-function Apartment() {
+function Apartment(props) {
   return (
     <Link to ="/flat">
     <div className='apartment'>
-    <img src = "https://picsum.photos/300/200" alt = "titre de l'image"></img>
-    <div className = "apartement__subtitle">Titre de la location </div>
-    
-   
+    <img src = {props.image} alt = "titre de l'image"></img>
+    <div className = "apartement__subtitle">{props.title} </div>
     </div>
     </Link>
   )
--- src/components/ApartmentGrid.jsx
+++ src/components/ApartmentGrid.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import "./ApartmentGrid.scss";
-import Apartement from "./Apartment.jsx"
+import Apartment from "./Apartment.jsx";
+import database from '/src/database.json';
 
 function ApartmentGrid() {
+  const [apartments, setApartments] = useState([]);
+
+  useEffect(() => {
+    fetchApartments();
+  }, []);
+
+  function fetchApartments() {
+    setApartments(Object.values(database)); // Convertit les objets d'appartements en un tableau
+  }
+
   return (
     <div className="grid">
-
-    <Apartement/>
-    <Apartement/>
-    <Apartement/>
-    <Apartement/>
-    
+      {apartments.map((apartment, index) => (
+        <Apartment title={apartment.title} 
+        image = {apartment.cover}
+        />
+      ))}
     </div>
   );
 }
